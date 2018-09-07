c0a98c8d75eda40a065486ef597277bc9c284ce1
Leonard Krämer <leonard.kraemer@uni-konstanz.de>
2018-09-07 18:29:24 +0200

    version 2 - frosted glass

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@
     function renderCircle(context, center, radius, color, startAngle, endAngle) {
         context.save();
         context.beginPath();
-        context.globalAlpha=0.4;
+        context.globalAlpha=0.6;
         context.fillStyle = color;
         context.arc(center.x, center.y, radius, startAngle, endAngle);
         context.lineTo(center.x, center.y)
@@ -61,7 +61,7 @@
         context.shadowOffsetX = 3;
         context.shadowOffsetY = 3;
         context.fillText(text, x, y);
-        context.globalCompositeOperation = 'difference';
+        //context.globalCompositeOperation = 'difference';
         context.globalAlpha=1;
         context.font = textSize + "px helvetica";
         context.textAlign = "center";
@@ -88,12 +88,54 @@
         // Clear canvas
         ctxContent.clearRect(0, 0, ctxContent.canvas.width, ctxContent.canvas.height);
         
-        // Draw the minute/hour circle
-        renderCircle(ctxContent, center, watchRadius, "#CCCCCC", 3/2 * Math.PI, 2 * Math.PI / 60 * (minute + second / 60) - 0.5 * Math.PI);
-        //renderCircle(ctxContent, center, watchRadius, "#CCCCCC",2 * Math.PI / 60 * (minute + (second - 1) / 60) - 0.5 * Math.PI, 3/2 * Math.PI);
-
         // Draw the text for date
-        renderText(ctxContent, hour, center.x, center.y + 25 , 300, "#cccccc");
+        //renderText(ctxContent, hour, center.x, center.y + 25 , 300, "#cccccc");
+        
+        // Draw the minute/hour circle
+        //renderCircle(ctxContent, center, watchRadius, "#CCCCCC", 3/2 * Math.PI, 2 * Math.PI / 60 * (minute + second / 60) - 0.5 * Math.PI);
+        //renderCircle(ctxContent, center, watchRadius, "#CCCCCC",2 * Math.PI / 60 * (minute + (second - 1) / 60) - 0.5 * Math.PI, 3/2 * Math.PI);
+        var x = 180 + 180 * Math.sin(Math.PI * 2 * (minute + second / 60) / 60);
+        var y = 180 + 180 *  Math.sin(Math.PI * 2 * (minute + second / 60 - 15) / 60);
+        document.getElementById("svg").innerHTML = '<defs>' +
+	    '<filter id="f1" x="-50%" y="-50%" width="200%" height="200%">' +
+	    '  <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="gaussOut" />' +
+	    '  <feColorMatrix result="matrixOut" in="gaussOut" type="matrix"' +
+    	'		values="0 0 0 0 0'+
+	    '				0 0 0 0 0'+
+	    '               0 0 0 0 0'+
+	    '               0 0 0 1 0" />' +
+	    '  <feOffset result="offOut" in="matrixOut" dx="3" dy="3" />' +
+	    '  <feBlend in="gaussOut" in2="offOut" mode="normal" />' +
+	    '</filter>' +
+	    '<clipPath id="c">' +
+	    '  <path d="M 180 0' +
+		  '			A 180 180 0 1 1 ' + x + ' ' + y +
+		  '			L 180 180 Z" />' +
+	   ' </clipPath>' +
+	   ' <clipPath id="notc">' +
+	   '  <path d="M ' + x + ' ' + y +
+		  '			A 180 180 0 1 1 180 0' +
+		  '			L 180 180 Z" />' +
+	   ' </clipPath>' +
+	   ' <filter id="f2" x="0" y="0" width="200%" height="200%">' +
+	   ' <feColorMatrix result="matrixOut" in="SourceGraphic" type="matrix"' +
+       '	    values="0 0 0 0 0'+
+	    '				0 0 0 0 0'+
+	    '               0 0 0 0 0'+
+	    '               0 0 0 1 0" />' +
+	   '   <feOffset result="offOut" in="matrixOut" dx="3" dy="3" />' +
+	   '   <feBlend in="SourceGraphic" in2="offOut" mode="normal" />' +
+	  '  </filter>' +
+	  '</defs>' +
+	  '<text text-anchor="middle" x="180" y="280" font-family="helvetica" filter="url(#f2)" font-size="300px" clip-path="url(#notc)" fill="lightgrey">' + hour + '</text>' +
+	  
+	  '<text text-anchor="middle" x="180" y="280" font-family="helvetica" filter="url(#f1)" clip-path="url(#c)" font-size="300px" fill="lightgrey">' + hour + '</text>' +
+	  '<path d="M 180 0' +
+	  '			A 180 180 0 1 1 ' + x + ' ' + y +
+	  '			L 180 180 Z" fill="lightgrey" fill-opacity="0.4" />"'  +
+	  '<line x1="180" y1="0" x2="180" y2="180" stroke="black" stroke-width="2" stroke-opacity="0.4"/>"'  +
+	  '<line x1="180" y1="180" x2="'+ x +'" y2="'+ y+'" stroke-width="2" stroke="black" stroke-opacity="0.4"/>"'
+
         //document.getElementById("hour").innerHTML = hour;
         animTimeout = setTimeout(function() {
             animRequest = window.requestAnimationFrame(drawNormalWatch);
